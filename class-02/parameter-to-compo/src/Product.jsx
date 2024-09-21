7e3d6bfa712315daea8afac179118fbfca646ff0
Muhammad Saad <149876788+MuhammadSaad38@users.noreply.github.com>
2024-09-21 14:59:08 +0500

    array & object used as a props

--- class-02/parameter-to-compo/src/Product.jsx
+++ class-02/parameter-to-compo/src/Product.jsx
@@ -3,7 +3,11 @@ export default function Product(props){
         <>
         <h3>{props.title}</h3>
         <h5>{props.price}</h5>
-        <h3>{props.features}</h3>
+        <p>{props.feature1}</p>
+        <p>{props.feature2.a}</p>
+        <p>{props.feature2.b}</p>
+        <p>{props.feature2.c}</p>
+
         </>
     )
 }
--- class-02/parameter-to-compo/src/ProductTab.jsx
+++ class-02/parameter-to-compo/src/ProductTab.jsx
@@ -1,11 +1,11 @@
-import Product from "./Product";
+ import Product from "./Product";
 
 export default function ProductTab(){
     return(
         <>
-        <Product title="phone" price={2000} />
-        <Product title="laptop" price={5000}/>
-        <Product title="pen" price={7000}/>
+        <Product title="phone" price={2000} feature1 = {["high-tech","expensive","Zeal-Zest"]} feature2 = {{a:"high-tech", b :"expensive", c:"Zeal-Zest"}} />
+        <Product title="laptop" price={5000} feature1 = {["high-tech","expensive","Zeal-Zest"]} feature2 = {{a:"high-tech", b :"expensive", c:"Zeal-Zest"}}/>
+        <Product title="pen" price={7000} feature1 = {["high-tech","expensive","Zeal-Zest"]} feature2 = {{a:"high-tech", b :"expensive", c:"Zeal-Zest"}}/>
         </>
     )
 }
