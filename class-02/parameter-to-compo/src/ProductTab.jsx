4ef5879fcc705f63a46052f0211b39e8f446ec27
Muhammad Saad <149876788+MuhammadSaad38@users.noreply.github.com>
2024-09-28 12:01:00 +0500

    learning array rendering

--- class-02/parameter-to-compo/src/ProductTab.jsx
+++ class-02/parameter-to-compo/src/ProductTab.jsx
@@ -1,11 +1,15 @@
  import Product from "./Product";
 
 export default function ProductTab(){
+    // let option = ["high-tech","expensive","Zeal-Zest"]
     return(
         <>
-        <Product title="phone" price={2000} feature1 = {["high-tech","expensive","Zeal-Zest"]} feature2 = {{a:"high-tech", b :"expensive", c:"Zeal-Zest"}} />
-        <Product title="laptop" price={5000} feature1 = {["high-tech","expensive","Zeal-Zest"]} feature2 = {{a:"high-tech", b :"expensive", c:"Zeal-Zest"}}/>
-        <Product title="pen" price={7000} feature1 = {["high-tech","expensive","Zeal-Zest"]} feature2 = {{a:"high-tech", b :"expensive", c:"Zeal-Zest"}}/>
+        {/* <Product title="phone" price={2000} feature = {option}/>
+        <Product title="laptop" price={5000} feature = {option}/>
+        <Product title="pen" price={7000} feature = {option} /> */}
+        <Product title="phone" price={2000} />
+        <Product title="laptop" price={5000}/>
+        <Product title="pen" price={7000} />
         </>
     )
 }
